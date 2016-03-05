448e6304c731cebc93f2947c4a9f4dc1eed5d612
Mark Anthony T. Montecillo <flintlook.asher@gmail.com>
2016-03-06 01:06:12 +0800

    If Statement

---
+++ if-statement.js
@@ -0,0 +1,7 @@
+var fruit;
+var fruit = 'orange';
+if (fruit > 5) {
+console.log('The fuit name has five characters or less.');
+} else {
+console.log('The fruit name has more than five characters.');
+}
